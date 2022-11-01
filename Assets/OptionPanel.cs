a3955e507fc9e052b431a53a2495287cc6232b65
草蛇 <blackzxz77@gmail.com>
2022-11-02 01:40:41 +0800

    add fonts folder

---
+++ Assets/OptionPanel.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class OptionPanel : MonoBehaviour
+{
+    public TMP_Text t_BgmVolume;
+    public TMP_Text t_SeVolume;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
